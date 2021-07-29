14ce57573715365de7f2cb75f1745cd993d5bff9
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-29 10:55:26 -0500

    Added login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,12 +4,33 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
 	Query: {
 		users: async () => {
+			// Returns all users
 			return User.find({});
 		},
 	},
 	Mutation: {
 		createUser: async (parent, args) => {
 			const user = await User.create(args);
+			const token = signToken(user);
+
+			// Returns token and user information
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+
+			if (!user) {
+				throw new AuthenticationError(
+					"No user found with this email address"
+				);
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+
+			if (!correctPw) {
+				throw new AuthenticationError("Incorrect credentials");
+			}
+
 			const token = signToken(user);
 
 			return { token, user };
