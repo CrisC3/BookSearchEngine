01cfcbfaa92d20f66e22af6c3df43addb95c9da0
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-28 21:15:27 -0500

    Added console log to display graphql URL path

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const path = require('path');
+const path = require("path");
 
-const db = require('./config/connection');
+const db = require("./config/connection");
 // const routes = require('./routes');
 const { typeDefs, resolvers } = require("./schemas");
 
@@ -10,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+	typeDefs,
+	resolvers,
 });
 
 server.applyMiddleware({ app });
@@ -20,12 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 // app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+	app.listen(PORT, () => {
+		console.log(`🌍 Now listening on localhost:${PORT}`);
+		console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+	});
 });
