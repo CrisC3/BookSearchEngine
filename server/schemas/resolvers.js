00f0bfc951f99062068d927974f53ec11e7771b5
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-07-29 10:01:54 -0500

    Added createUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,19 @@
 const { User } = require("../models");
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find({});
-        }
-    }
+	Query: {
+		users: async () => {
+			return User.find({});
+		},
+	},
+	Mutation: {
+		createUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+
+			return { token, user };
+		},
+	},
 };
 
 module.exports = resolvers;
